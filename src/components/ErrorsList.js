68f46fceecd007091acc286004e48dc8482f34f1
Luke Harris <luke.harris@btopenworld.com>
2022-02-11 16:30:48 +0000

    Removed li style bullets

--- src/components/ErrorsList.js
+++ src/components/ErrorsList.js
@@ -1,6 +1,6 @@
 export default function ErrorsList({ errors }) {
   return (errors.length !== 0) 
-    ? (<div className="errors">
+    ? (<div className="error-message">
         <h3>Errors</h3>
         <ul>
           {errors.map( (error, index) => <li key={index}>{error.message}</li>)}
